b42c94d68a7498782deeff99b73de6862b7e291b
ghanmi-hamza <hamza.ghanmi56@gmail.com>
2021-04-28 00:36:47 +0100

    first commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template, request, redirect
 
 app = Flask(__name__)
 
-def factors(n):
+"""def factors(n):
     li = []
     while n > 1:
         for i in range(2, int(n + 1)):
@@ -10,7 +10,19 @@ def factors(n):
                 n /= i
                 li.append(i)
                 #yield i
-    return(li)
+    return(li)"""
+def factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
 
 
 
